45846b0c782024a6b056a1427659d7c2a264003a
Julian Buechel <julianb@ini.uzh.ch>
2019-04-06 23:17:03 +0200

    Added capsule convolution objects (keras)

--- CapsuleLayer.py
+++ CapsuleLayer.py
@@ -20,38 +20,51 @@ class CapsuleLayer:
                       PrimaryCaps and 10 for the DigiCaps.
         vec_length:   Corresponds to the depth of each
                       capsule.
-        output_size:  Corresponds to the dimension of each
-                      capsule output after the convolution.
+        kernel_size:  Kernel size for convolution.
                       In the paper, the PrimaryCaps each
                       perform convolution with a 9x9 kernel
-                      on a 20x20 input, resulting in 6x6
-                      (output_size)
+                      on a 20x20 input, resulting in 6x6.
         name:         Name of the capsule layer.
         c_type:       Either "FC" or "CONV"
     In total the output dimension should be
     [batch_size, 32 x 6 x 6, 8]"""
 
-    def __init__(self, num_capsules, vec_length, output_size, name, c_type, isPrimary = False):
+    def __init__(self, num_capsules, vec_length, kernel_size, name, c_type, isPrimary = False):
 
-        super(CapsuleLayer, self).__init__()
         # Store the information in fields
         self.num_capsules = num_capsules
         self.vec_length = vec_length
-        self.output_size = output_size
+        self.kernel_size = kernel_size
         self.name = name
         self.isPrimary = isPrimary
         self.c_type = c_type
 
+        
+        self.convs = []
+        for _ in range(self.num_capsules):
+            conv = tf.keras.layers.Conv2D(filters=self.vec_length, kernel_size=self.kernel_size,strides=(2,2),activation='relu')
+            self.convs.append(conv)
 
-
+    # TODO Make stride variable
     def __call__(self, input):
 
-        # 1) Compute the output for each pixel with size
-        # [batch_size, num_capsules x output_size x output_size x vec_length]
-        # 2) Squash the output of each vector using the total input to the capsule.
-        # Apparently, this is not the case for the primary caps.
-        # Simply check isPrimary field.
+    # 1) Compute the output for each pixel with size
+    # [batch_size, num_capsules x output_size x output_size x vec_length]
+    # 2) Squash the output of each vector using the total input to the capsule.
+    # Apparently, this is not the case for the primary caps.
+    # Simply check isPrimary field.
 
-        print(tf.shape(input))
+        if(self.c_type == "CONV"):
+            if(self.isPrimary):
+                print('Is primary')
+
+            else:
+                raise ValueError("Convolutional non-primary capsule not implemented yet.")
+
+        elif(self.c_type == "FC"):
+            print('Fully connected')
+
+        else:
+            raise ValueError("Type %s is not supported." % self.c_type)
         
 
--- net.py
+++ net.py
@@ -1,5 +1,6 @@
 #%%
 
+import importlib
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
@@ -8,7 +9,7 @@ import CapsuleLayer
 
 
 #%%
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 EPOCHS = 1
 
 dataset, info = tfds.load('mnist', with_info=True, as_supervised=True)
@@ -23,27 +24,11 @@ mnist_train = mnist_train.map(convert_types).shuffle(10000).batch(BATCH_SIZE)
 mnist_test = mnist_test.map(convert_types).batch(BATCH_SIZE)
 
 #%%
+importlib.reload(CapsuleLayer)
+#importlib.reload(MaskingLayer)
+#importlib.reload(DecodingLayer)
 
-"""
-Class CapsNet will do the initial convolution, the routing, the FC layers
-and keep track of the intermediate weights W{i,j}.
-It is not yet supported that we have multiple conv2d units at the beginning.
-
-Arguments:
-  batch_size:     Batch size.
-  capsule_struct: List of triple, containing information about each capsule
-                  layer to be added to the network.
-                  Form: [(num_capsules, vec_length, output_size),...,]
-  conv2d_struct:  Quadruple of filter_size, kernel_size, stride and activation.
-  fc_struct:      List of fully connected layers. First element in tuple is size,
-                  second one is activation.
-                  This struct is passed to the decoding layer.
-                  Note that the last shape is inferred from the input shape.
-                  e.g. [(512, "relu"),(1024, "relu")] will be translated to
-                  [(512, "relu"),(1024, "relu"), (784, "sigmoid")]
-"""
-
-mnist_capsule_struct = [(32, 8, 6), (16, 10)]
+mnist_capsule_struct = [(32, 8, 9), (16, 10)]
 mnist_conv2d_struct =  (256, 9, 1, "relu")
 mnist_fc_struct = [(512, "relu"),(1024, "relu")]
 
@@ -57,8 +42,8 @@ class CapsNet:
     self.n_capsules = len(capsule_struct)
 
     # Initial Convolution
-    self.conv1 = tf.keras.layers.Conv2D(self.conv2d_struct[0],self.conv2d_struct[1],
-                                        self.conv2d_struct[2],activation=self.conv2d_struct[3])
+    self.conv1 = tf.keras.layers.Conv2D(filters=self.conv2d_struct[0],kernel_size=self.conv2d_struct[1],
+                                        strides=self.conv2d_struct[2],activation=self.conv2d_struct[3])
 
     # Capsule Layers
     self.capsule_layers = []
@@ -66,12 +51,12 @@ class CapsNet:
       t = capsule_struct[i]
       name = "caps" + str(i)
       c_type = "CONV" if (len(t) == 3) else "FC"
-      output_size = t[2] if (len(t) == 3) else 0
+      kernel_size = t[2] if (len(t) == 3) else 0
       vec_length = t[1]
       num_c = t[0]
       isPrimary = (i==0)
 
-      self.capsule_layers.append(CapsuleLayer.CapsuleLayer(num_c, vec_length, output_size,
+      self.capsule_layers.append(CapsuleLayer.CapsuleLayer(num_c, vec_length, kernel_size,
                                             name, c_type, isPrimary))
 
 
@@ -91,22 +76,20 @@ class CapsNet:
     conv1_out = self.conv1(x)
     caps_out = self.capsule_layers[0](conv1_out)
 
-    print(tf.shape(caps_out))
-
     for capsLayer in self.capsule_layers[1:]:
       caps_out = capsLayer(caps_out)
 
-    # Feed into Masking Layer
 
-    # Feed into Decoding Layer
-
-    # Return Masking sol and Decoding sol
     
 
 capsnet = CapsNet(BATCH_SIZE, mnist_capsule_struct, mnist_conv2d_struct, mnist_fc_struct)
 
+@tf.function
+def train_step(image, label):
+  #with tf.GradientTape() as tape:
+  out = capsnet(image)
 
-#%%
+
 
 # Note: Image will have tensor dimension [batch_size, 28, 28, 1]
 #       Label will have dimension [batch_size]
@@ -114,7 +97,11 @@ capsnet = CapsNet(BATCH_SIZE, mnist_capsule_struct, mnist_conv2d_struct, mnist_f
 for _ in range(EPOCHS):
     for idx,(image, label) in enumerate(mnist_train):
       if(idx < 1):
-        capsnet(image)
+        train_step(image, label)
         
 
 
+#%%
+
+
+#%%
